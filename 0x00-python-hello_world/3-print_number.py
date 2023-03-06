a7a91785d1e90f88de3856074b54096f118ddf19
Joseph Njoroge <91351673+kimenyu@users.noreply.github.com>
2023-03-06 16:07:38 +0300

    Update 3-print_number.py

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 number = 98
 print(f"{number:d} Battery street")
+#The expression {number:d} is a format specifier that indicates that the value of number should be formatted as a decimal integer (d stands for decimal). Since number is already an integer, the format specifier doesn't change its value or type. Therefore, the output of the code is simply "98 Battery street".
