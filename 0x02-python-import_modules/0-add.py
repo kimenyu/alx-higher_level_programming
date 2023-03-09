40eaeb5fe339cffd9f4cb523c52e92b35dd9cc82
kimenyu <njorogekimenyu@gamil.com>
2023-03-09 02:22:48 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import add_0
 def add(a, b):
     a = 1
     b = 2
     ans = a + b
-    return(ans)
-print(f"1 + 2 = {str(int(add('a', 'b'))).format(add)}")
+    return((f"1 + 2 = {str(int(add('a', 'b'))).format(add)}")
