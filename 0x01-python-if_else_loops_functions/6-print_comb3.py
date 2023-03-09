22f88d42ad4366bec5d757283f6a072615fef8fd
kimenyu <njorogekimenyu@gamil.com>
2023-03-08 18:59:04 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for n in range(0, 9):
-    for i in range(n + 1, 10):
-        if n == 8 and i == 9:
-            print("{:d}{}".format(n, i))
-        print("{}{}".format(n, i), end=", ")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
