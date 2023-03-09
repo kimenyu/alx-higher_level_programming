84b6bf1f8b6517a37c4ae6d995fdf2863013e842
kimenyu <njorogekimenyu@gamil.com>
2023-03-08 22:37:17 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 10
+    b = 5
+    ans = a + b
+    return (ans)
+print(f"10 + 5 = {str(int(add('a', 'b'))).format(add)}")
+
+def sub(a, b):
+    a = 10
+    b = 5
+    ans = a - b
+    return(ans)
+print(f"10 - 5 = {str(int(sub('a', 'b'))).format(sub)}")
+
+def mul(a, b):
+    a = 10
+    b = 5
+    ans = 10 * 5
+    return(ans)
+print(f"10 * 5 = {str(int(mul('a', 'b'))).format(mul)}")
+
+def div(a, b):
+    a = 10
+    b = 5
+    ans = 10/5
+    return(ans)
+print(f"10 / 5 = {str(int(div('a', 'b'))).format(div)}")
