135582e2118166d2cd8af9f26d3116b80dd5bf66
kimenyu <njorogekimenyu@gamil.com>
2023-03-09 02:39:31 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     from add_0.py import add_0
     a = 1
     b = 2
-    print((f"{a} + {b} = {str(int(add('a', 'b'))).format(add)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
