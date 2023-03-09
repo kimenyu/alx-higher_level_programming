e40352f0de476340eb5e265b1b228f92de0f985b
kimenyu <njorogekimenyu@gamil.com>
2023-03-08 18:50:18 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range( 97, 123):
+for i in range(97, 123):
     if chr(i) not in ['e', 'q']:
-        print(chr(i).format(i) , end='')
+        print(ord(i).format(i), end='')
