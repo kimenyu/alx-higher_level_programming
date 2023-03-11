c34f11fdca19f4cae85b4bf711a1cee4e6c93d56
kimenyu <njorogekimenyu@gamil.com>
2023-03-11 00:19:15 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     my_list = []
     for i in my_list:
-        print(i.format(my_list))
-print_list_integer(my_list=[])
+        print(i)
+print_list_integer(f"my_list=[].format(i)")
