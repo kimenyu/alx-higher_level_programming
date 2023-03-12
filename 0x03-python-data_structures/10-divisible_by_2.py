aca342ae29ae257354b1fe5d5ff7a96aca1b448c
kimenyu <njorogekimenyu@gamil.com>
2023-03-11 22:01:51 -0800

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if my_list[idx] % 2 == 0:
-        return my_list = True
+        return my_list.append(True)
     else:
-        return my_list = False
+        return my_list.append(False)
