13d76ba23dbe753e8c256bb68d8b373324ca2801
kimenyu <njorogekimenyu@gamil.com>
2023-03-12 08:04:56 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list=[]
-    new = list(my_list)
+    new = my_list.copy()
     id idx < 0  or idx >= len(my_list):
         return new
     else:
