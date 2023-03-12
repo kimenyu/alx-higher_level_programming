a52a985bb48e881ab273ccf48ffbfd4acba85803
kimenyu <njorogekimenyu@gamil.com>
2023-03-12 05:17:44 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new = my_list.copy()
+    new = list(my_list)
     my_list[idx] = element
     if idx < 0 or idx >= len(my_list):
         return new
