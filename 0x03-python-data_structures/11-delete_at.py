8e0f3cad4db0536c1eb6bd93fcf30a3eeb3f0cfd
kimenyu <njorogekimenyu@gamil.com>
2023-03-11 23:19:21 -0800

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    my_list = []
+    new_list = my_list.remove(idx)
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list.remove('idx')
-        return my_list
+        return new_list
