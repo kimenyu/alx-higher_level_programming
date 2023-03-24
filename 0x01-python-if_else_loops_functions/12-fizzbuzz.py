fc0dcd794339fdc422b489c29bbd7663b649aeb1
kimenyu <njorogekimenyu@gamil.com>
2023-03-24 04:05:13 -0700

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,8 +5,8 @@ def fizzbuzz():
             print('Fizz', end=' ')
         elif i % 5 == 0:
             print('Buzz', end=' ')
-        elif i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz', end=' ')
-        else:
-            print(i, end=' ')
+            if i % 3 == 0 and i % 5 == 0:
+                print('FizzBuzz', end=' ')
+            else:
+                print(i, end=' ')
     return i
