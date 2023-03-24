c6f78c849c4ea8445524c00aca2cf9723fe4297b
kimenyu <njorogekimenyu@gamil.com>
2023-03-24 02:26:11 -0700

    9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     digit = abs(number) % 10
     last_digit = -digit
-    if last_digit < 0:
+    if number < 0:
         return last_digit
     else:
         return digit
