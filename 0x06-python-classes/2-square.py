a2cb011ab8fdc668125361401fad21fb1576cd96
kimenyu <njorogekimenyu@gamil.com>
2023-03-29 02:31:06 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,10 @@ class Square():
 
     def __init__(self, size=0):
         """ initialize the class with a private square attribute"""
-        self.__size = size
+
         try:
             if (typeof(size) is not int):
                 raise TypeError("size must be an integer")
             else if(size < 0):
                 raise ValueError("size must be >= 0")
-            self.__size = size
+        self.__size = size
