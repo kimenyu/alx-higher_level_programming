fd6a761c0630b1daa0cf1ea756b37dfba9e8de6d
JOSEPH <njorogekimenyu@gmail.com>
2023-04-04 01:12:01 -0700

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -37,7 +37,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Setter sets width if int > 0 """
-        if(type(value) is not int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -51,7 +51,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Setter sets height if int > 0 """
-        if(type(value) is not int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -67,10 +67,9 @@ class Rectangle:
             return 0
         return (2 * self.__width) + (2 * self.height)
 
-
     def __str__(self):
         """ Prints rectangle with #'s """
         if self.__width == 0 or self.__height == 0:
             return ""
-        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
-        return pic
+        pen = "\n".join(["#" * self.__width for rows in range(self.__height)])
+        return pen
