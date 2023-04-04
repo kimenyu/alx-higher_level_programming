f71c46b762bbd0d587c627d52a011cfd28d6eb4e
JOSEPH <njorogekimenyu@gmail.com>
2023-04-03 23:24:50 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,50 @@
 #!/usr/bin/python3
 """
-A module class that defines rectangle
+This module contains a Rectangle class
 """
 
 
 class Rectangle:
-    """ Creating a private instance"""
+     """ This class defines a Rectangle. It takes 2 attributes
+    width and height which are both integers
+    """
     def __init__(self, width=0, height=0):
+        """ initializes a Rectangle instance
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        """
         self.__width = width
         self.__height = height
 
-    """Property to retreive it"""
+    
     @property
     def width(self):
+        """ This should get the width"""
     	return self.__width
     
 
-    """creating a property setter for width"""
+    
     @width.setter
     def width(self, value):
+        """ This assigns value to width"""
         if(type(value) is not int):
             raise TypeError("width must be an integer")
         elif(value < 0):
             raise ValueError("width must be >= 0")
             self.__width = value
 
-    """Creating a property to retrieve it"""
 
     @property
     def height(self):
+        """ This retrieves the private attribute height"""
     	return self.__height
 
-    """Creating  a property setter for height"""
+    
 
     @height.setter
     def height(self, value):
+        """ This asigns a new value to attribute height"""
         if(type(value) is not int):
             raise TypeError("height must be an integer")
         elif(value < 0):
