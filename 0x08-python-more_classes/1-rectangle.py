b9e180bf8fa1ae96381918bc6dd2519e809a37c6
JOSEPH <njorogekimenyu@gmail.com>
2023-04-04 00:20:33 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """class Rectangle  to define a rectangle"""
 class Rectangle:
-    """ Creating a private instance"""
+      """
+    Defines class rectangle with private attribute width and height
+    Args:
+        width (int): width
+        height (int): height
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+    """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
