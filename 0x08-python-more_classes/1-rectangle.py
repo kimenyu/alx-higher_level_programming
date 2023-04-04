a87f4de5c43372e9057425bfcd6708856267da93
JOSEPH <njorogekimenyu@gmail.com>
2023-04-04 00:24:37 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,35 +1,55 @@
 #!/usr/bin/python3
-"""class Rectangle  to define a rectangle"""
+"""
+Module 2-rectangle
+Contains class Rectangle
+with private attribute width and height
+"""
+
+
 class Rectangle:
-      """Creating a rectangle class"""
+    """
+    Defines class rectangle with private attribute width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+    """
     def __init__(self, width=0, height=0):
+        """ Initialize rectangles """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Property to retrieve it"""
+        """ Getter returns width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """creating a property setter for width"""
-        if(type(value) is not int):
+        """ Setter sets width if int > 0 """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Creating a property to retrieve it"""
+        """ Getter returns height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Creating  a property setter for height"""
-        if(type(value) is not int):
+        """ Setter sets height if int > 0 """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
