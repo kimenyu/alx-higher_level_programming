edaa721b1a6fe6a23393a2e637b3f7173245c541
JOSEPH <njorogekimenyu@gmail.com>
2023-04-04 00:18:28 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     @property
     def width(self):
         """Property to retrieve it"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -18,12 +18,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """Creating a property to retrieve it"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -32,4 +32,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
