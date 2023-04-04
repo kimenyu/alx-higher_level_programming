6379b4f158064d62f8b7d49eba2a7c9b6fe9aee9
JOSEPH <njorogekimenyu@gmail.com>
2023-04-03 23:36:13 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,61 +1,42 @@
 #!/usr/bin/python3
-"""
-This module contains a Rectangle class
-"""
-
-
+"""class Rectangle  to define a rectangle"""
 class Rectangle:
-    """ This class defines a Rectangle. It takes 2 attributes
-    width and height which are both integers
-    """
+    """ Creating a private instance"""
     def __init__(self, width=0, height=0):
-        """ initializes a Rectangle instance
+        self.__width = width
+        self.__height = height
 
-        Args:
-            width: width of the rectangle
-            height: height of the rectangle
-        """
-
-        if isinstance(width, int):
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = width
-        else:
-            raise TypeError("width must be an integer")
-
-        if isinstance(height, int):
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = height
-        else:
-            raise TypeError("height must be an integer")
 
     @property
     def width(self):
-        """ This should get the width"""
-        return self.__width
+    	"""Property to retreive it"""
+    	return self.__width
+
+
 
     @width.setter
     def width(self, value):
-        """ This assigns value to width"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
-        else:
+    	"""creating a property setter for width"""
+        if(type(value) is not int):
             raise TypeError("width must be an integer")
+        elif(value < 0):
+            raise ValueError("width must be >= 0")
+            self.__width = value
+
+
 
     @property
     def height(self):
-        """ This retrieves the private attribute height"""
-        retrun self.__height
+    	"""Creating a property to retrieve it"""
+    	return self.__height
+
+
 
     @height.setter
     def height(self, value):
-        """ This asigns a new value to attribute height"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
-        else:
+    	"""Creating  a property setter for height"""
+        if(type(value) is not int):
             raise TypeError("height must be an integer")
+        elif(value < 0):
+            raise ValueError("height must be >= 0")
+            self.__height = value
