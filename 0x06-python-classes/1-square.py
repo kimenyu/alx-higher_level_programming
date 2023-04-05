5f146b5e50e64fe8a7ebc0e361e49b322f28ffed
JOSEPH <njorogekimenyu@gmail.com>
2023-04-05 05:10:44 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defining a class based on 0-square.py
+
+"""
+class Square:
+    """
+    defining a class with a private inatsnce attribute size
+    """
+    def __init__(self, size):
+        self.size = size
