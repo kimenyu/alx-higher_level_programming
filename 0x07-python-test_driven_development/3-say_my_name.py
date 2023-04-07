8d7d447af8827a4ffb661563facae8c1a9755cf6
JOSEPH <njorogekimenyu@gmail.com>
2023-04-07 01:39:41 -0700

    testing

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,9 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is [full name]"
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
