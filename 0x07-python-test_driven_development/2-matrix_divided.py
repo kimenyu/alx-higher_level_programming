49e8d4cd4125d83df5b47d05aa7b6b5a4754fd5d
JOSEPH <njorogekimenyu@gmail.com>
2023-04-07 01:24:13 -0700

    test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,22 +1,35 @@
-#!/usr/python3
-"""matrix divider module"""
+#!/usr/bin/python3
+"""
+Module 2-matrix_divided
+Contains method that divides all elements of a matrix and returns new matrix
+Requires same size lists of ints or floats, and max two decimal places
+"""
+
+
 def matrix_divided(matrix, div):
-     """divides a matrix all elements by divider
     """
-    if not isinstance((element,  (int, float)) for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(row) != len(matrix[0] for row in matrix):
-		raise TypeError("Each row of the matrix must have the same size")
-	if not isinstance(div, (int, float)):
-		raise TypeError("div must be a number")
-	if div == 0:
-		raise ZeroDivisionError("division by zero")
+    Returns new matrix with dividends
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
-	new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(round(element / div, 2))
-        new_matrix.append(new_row)
+    msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
+        raise TypeError(msg)
 
+    new_matrix = []
+    samelen = len(matrix[0])
+    for lists in matrix:
+        if type(lists) is not list:
+            raise TypeError(msg)
+        if len(lists) != samelen:
+            raise TypeError("Each row of the matrix must have the same size")
+        newlist = []
+        for i in lists:
+            if not isinstance(i, (int, float)):
+                raise TypeError(msg)
+            newlist.append(round(i/div, 2))
+        new_matrix.append(newlist)
     return new_matrix
