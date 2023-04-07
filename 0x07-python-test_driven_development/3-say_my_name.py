5b5f8e46a143ed10b2a6bfe8099c62aa4ff87aa3
JOSEPH <njorogekimenyu@gmail.com>
2023-04-07 01:43:43 -0700

    testing

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''say my name module'''
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is [full name]"
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    return ("My name is {} {}".format(first_name, last_name))
