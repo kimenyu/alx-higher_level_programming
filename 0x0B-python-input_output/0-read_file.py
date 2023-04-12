23c069b090640ec3514224bd173d74be2b6ad08a
JOSEPH <njorogekimenyu@gmail.com>
2023-04-12 03:19:33 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-""" A fuction that reads a  text file"""
+
+""" 
+A fuction that reads a  text file
+"""
+
 def read_file(filename=""):
+
+    """
+    Using with kyword
+    """
+
     with ("my_file_0.txt", encoding="utf-8") as myFile:
         print(myFile.read())
