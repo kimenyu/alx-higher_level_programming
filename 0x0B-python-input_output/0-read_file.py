ed6d4225ebc3d6c645d209ec3c42056052299959
JOSEPH <njorogekimenyu@gmail.com>
2023-04-12 03:31:38 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,10 +5,5 @@ A fuction that reads a  text file
 """
 
 def read_file(filename=""):
-
-    """
-    Using with kyword
-    """
-
-    with open (filename, 'r', encoding="utf-8") as myFile:
+    with open (filename,"'r", encoding="UTF-8") as myFile:
         print(myFile.read(), end="")
