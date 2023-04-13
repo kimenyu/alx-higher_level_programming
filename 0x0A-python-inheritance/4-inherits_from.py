2061475efcf6a20303c99bf98288bb3fe97ced6e
JOSEPH <njorogekimenyu@gmail.com>
2023-04-13 02:17:08 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Returns True"""
+"""subclass checker"""
 
 
 def inherits_from(obj, a_class):
-    """Returns True"""
-    if issubclass and isinstance (obj, a_class):
-        return True
-    else:
+    """returns subclass yes"""
+    if type(obj) == a_class:
         return False
+    return issubclass(type(obj), a_class)
