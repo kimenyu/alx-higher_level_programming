7e836a9dce525f6d742bfda1e8da7ab3af46ae54
JOSEPH <njorogekimenyu@gmail.com>
2023-04-15 01:30:41 -0700

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """return true if inherited directly or indirectly"""
 
+
+
 def inherits_from(obj, a_class):
     """returns true"""
     if issubclass (type(obj), a_class) and type(obj) != a_class:
