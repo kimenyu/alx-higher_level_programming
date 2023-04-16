20600aed05e6f063870f3d5fe4e7c744051d9254
JOSEPH <njorogekimenyu@gmail.com>
2023-04-16 05:46:25 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,7 +17,7 @@ class Square:
         """instantiate"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("size must be >= 0")
 
         self.__size = value
