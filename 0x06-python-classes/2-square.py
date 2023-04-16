f5ba1741c3e2094d3b451acf281184b938be9cd3
JOSEPH <njorogekimenyu@gmail.com>
2023-04-16 05:26:04 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Create an empty class"""
+
+
+class Square:
+    """Instantiate size as private attribute """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter method"""
+        self.__size = size
+
+    @size.setter
+    def size(self, value):
+        """instantiate"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
