69721f4b66d8803fdc161a68b5d6515394c2f241
kimenyu <njorogekimenyu@gmail.com>
2023-04-17 19:03:36 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,13 +14,13 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-    def validator(self, value):
+    def validator(self,name, value):
         """validates"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if (name is 'width' or name is 'height') and value <= 0:
+        if (name == 'width' or name == 'height') and value <= 0:
             raise ValueError("{} must be > 0".format(name))
-        if (name is 'x' or name is 'y') and value < 0:
+        if (name == 'x' or name == 'y') and value < 0:
             raise ValueError("{} must be >= 0".format(name))
 
         
