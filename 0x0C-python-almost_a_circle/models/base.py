35af25cbe1c741eac9ea7366f4ece23034aebe25
kimenyu <njorogekimenyu@gmail.com>
2023-04-21 13:26:40 -0700

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """to json"""
         if list_dictionaries is  None:
-            return []
+            return "[]"
         else:
             data = json.dumps(list_dictionaries)
             return data
