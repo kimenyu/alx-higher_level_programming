e4713bba66f32c29599fa163088ccc6f74ce8b4d
kimenyu <njorogekimenyu@gmail.com>
2023-05-11 06:23:32 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""writing"""
+
+
+def write_file(filename="", text=""):
+    """weriting"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return(f.write(text)
