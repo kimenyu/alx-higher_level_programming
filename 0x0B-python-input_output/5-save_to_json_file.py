a820bc108c783e8b4fc19e32a0b79254ffbfb48d
kimenyu <njorogekimenyu@gmail.com>
2023-05-11 09:06:30 -0700

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""saves to json file"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """saves to json file"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        json.dump(my_obj, f) 
