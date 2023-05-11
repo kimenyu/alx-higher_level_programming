be8016c2592ca4558c6b66e3367e03a433bc9de1
kimenyu <njorogekimenyu@gmail.com>
2023-05-11 09:43:30 -0700

    rem

--- 0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import json
-"""loads an object from json file"""
-
-
-def load_from_json_file(filename):
-    """does it"""
-    with open(filename, mode='r', encoding='utf-8') as f:
-        return(json.load(f))
