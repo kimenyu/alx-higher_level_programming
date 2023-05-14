63cd709455bada08603e6150b4f8ad279ba9fad0
Joseph Njoroge <91351673+kimenyu@users.noreply.github.com>
2023-05-14 18:00:25 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,3 +19,19 @@ class Base:
     def to_json_string(list_dictionaries):
         """return json"""
         return json.dumps(list_dictionaries or [])
+    @classmethod
+    def save_to_file(cls, list_objs):
+        file_name = cls.__name__ + '.json'
+        my_list = []
+        if list_objs:
+            my_list = [obj.to_dictionary() for obj in list_objs]
+        with open(file_name, mode = 'w') as f:
+            f.write(cls.to_json_string(my_list))
+            #json.dump(my_list, f)
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            datan = json.loads(json_string)
+            return datan
