920e9d66b9771053bd7cf97ea557134852d964bd
kimenyu <njorogekimenyu@gmail.com>
2023-06-10 02:31:27 -0700

    0-\

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""function that reads a text file (UTF8) and prints it to stdout:"""
+"""reads a file"""
+
+
 def read_file(filename=""):
-    """with open"""
-    with open('filename=""', 'r', encoding='utf-8') as f:
-        f.read()
+    """reads a file"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end="")
