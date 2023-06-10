6307c251af13dd4eeb5db23cbf4b1d5bf0e66a95
kimenyu <njorogekimenyu@gmail.com>
2023-06-10 02:42:20 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,5 @@
 def read_file(filename=""):
     """reads a file"""
     with open(filename, mode='r', encoding='utf-8') as f:
-        #read_file = f.read()
-        #print(read_file, end="")
         for line in f:
             print(line, end="")
