84793694fd292d6f98be685dd3588cf7e8a66596
kimenyu <njorogekimenyu@gmail.com>
2023-06-10 01:58:37 -0700

    rem

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""rwwedas"""
-
-
-def read_file(filename=""):
-    """reads a file"""
-    with open(filename, mode="r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
