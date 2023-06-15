c5ca930550d46eed91da3317413beb604e486d21
kimenyu <njorogekimenyu@gmail.com>
2023-06-15 14:19:08 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Creating a class square"""
-
-
 class Square:
-    """square"""
+    """
+    creates a square object
+    """
     def __init__(self, size=0):
-        """instantiation"""
-        if not isinstance(size, int):
+        if(type(size) is not int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if(size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+        """
+        initializes instance of a square
+        Args:
+            __size(int): size of square
+            __position(tuple):position
+        """
     def area(self):
-        """returns the current square area"""
-        return (self.__size ** 2)
+        return(self.__size**2)
+    """
+    returns area of square based on size
+    """
