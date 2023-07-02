9695a60d66ba1aae37bcf082537dafe23b467016
kimenyu <njorogekimenyu@gmail.com>
2023-07-02 13:19:08 -0700

    rem

--- 0x03-python-data_structures/2-replace_in_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    new = list(my_list)
-    if idx < 0 or idx >= len(my_list):
-        return new
-    else:
-        my_list[idx] = element
-        return my_list
