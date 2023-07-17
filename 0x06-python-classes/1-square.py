605474973a182b9e62458151a0f9a0a375759bbb
kimenyu <njorogekimenyu@gmail.com>
2023-07-17 20:52:57 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""an empty class that defines a square
+"""
+
+
+class Square:
+    """class square with private instance atrribute size
+    """
+    def __init__(self, size):
+        """class atrribute
+
+        Args:
+            size (int): a private intance atrribute
+        """
+        self.__size = size
