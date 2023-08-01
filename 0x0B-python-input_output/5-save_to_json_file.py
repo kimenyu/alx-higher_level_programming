c44d420ae3b51d9b41971a71d7fa9bebb64dd633
kimenyu <njorogekimenyu@gmail.com>
2023-08-01 20:20:34 +0000

    rem

--- 0x0B-python-input_output/5-save_to_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""saves to json file"""
-import json
-
-
-def save_to_json_file(my_obj, filename):
-    """saves to json file"""
-    with open(filename, mode='w') as f:
-        json.dump(my_obj, f)
