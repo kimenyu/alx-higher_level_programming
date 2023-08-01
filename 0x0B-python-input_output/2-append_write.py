6a72b0aba5ce10bccde7aa4b61783b8fa6604536
kimenyu <njorogekimenyu@gmail.com>
2023-08-01 19:49:31 +0000

    rem

--- 0x0B-python-input_output/2-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""append"""
-
-
-def append_write(filename="", text=""):
-    """append"""
-    with open(filename, mode='a+', encoding='utf-8') as f:
-        return(f.write(text))
