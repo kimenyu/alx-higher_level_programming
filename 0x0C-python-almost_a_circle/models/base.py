7c8cc8b867e076b4c7677edc521fc4742f882168
kimenyu <njorogekimenyu@gmail.com>
2023-08-02 19:58:56 +0000

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,41 +1,14 @@
 #!/usr/bin/python3
-"""Creating a class"""
-import json
+"""first class"""
 
 
-class Base:
-    """private class attribute"""
+class Base(object):
+    """docstring for Base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """inits the class"""
-        if id is not None:
+	if id:
             self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-    @staticmethod
-    def to_json_string(list_dictionaries):
-        """return json"""
-        return json.dumps(list_dictionaries or [])
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """save to file"""
-        file_name = cls.__name__ + '.json'
-        my_list = []
-        if list_objs:
-            my_list = [obj.to_dictionary() for obj in list_objs]
-        with open(file_name, mode = 'w') as f:
-            f.write(cls.to_json_string(my_list))
-            #json.dump(my_list, f)
-
-    @staticmethod
-    def from_json_string(json_string):
-        """from json_string"""
-        if json_string is None:
-            return []
-        else:
-            datan = json.loads(json_string)
-            return datan
+	else:
+	    Base.__nb_objects += 1
+	    self.id = Base.__nb_object
