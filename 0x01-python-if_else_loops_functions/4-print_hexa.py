0105c6c50b87c47b87d8168f782bfb02e6cf2975
kimenyu <njorogekimnyu@gmail.com>
2023-10-13 15:00:35 +0000

    readme

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for i, element in enumerate(range(0, 98)):
-    print(f"{i+1}={hex(element).format(i)}")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,1 +1,1 @@
-0x01. Python - if/else, loops, functions
+if else statements
